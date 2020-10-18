a44682d17b7092b0a4bb4c7bf1c60841a5cfc9cd
dmvoro <dmvoro@gmail.com>
2020-10-18 23:45:56 +0300

    feat: rev int impl

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    var revString = n.toString();
+    var splitString = revString.split(""); 
+    var reverseArray = splitString.reverse();     
+    var joinArray = reverseArray.join(""); 
+    
+    
+    return parseInt(joinArray)
 }
